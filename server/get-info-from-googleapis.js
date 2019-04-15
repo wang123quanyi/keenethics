1b57a62c5134592cf16d7c39377fbd08851e54c6
Dmytro Panontko <dmytro.panontko@keenethics.com>
2019-04-15 17:58:47 +0300

    WIP

--- server/get-info-from-googleapis.js
+++ server/get-info-from-googleapis.js
@@ -4,17 +4,14 @@ const _ = require('lodash');
 
 const TEAM_SHEET_ID = spreadSheet.id;
 const CREDS_FILE = spreadSheet.securityFile;
-const scopes = ['https://www.googleapis.com/auth/drive.readonly'];
+const googleScopes = ['https://www.googleapis.com/auth/drive.readonly'];
 const TEAM_RANGE = spreadSheet.teamRange;
 const CAREERS_RANGE = spreadSheet.careersRange;
 
 const ranges = [TEAM_RANGE, CAREERS_RANGE];
 
+const TEN_MINUTES = 10 * 60 * 1000;
 const TITLE_COLUMN_INDEX = 0;
-
-let dataStore = null;
-let updateTime = null;
-const timeToLive = 10 * 60 * 1000; // 10min
 
 const arrayOfArraysToCollection = (arr) => {
   const properties = arr[TITLE_COLUMN_INDEX];
@@ -22,69 +19,91 @@ const arrayOfArraysToCollection = (arr) => {
     .map(v => _.fromPairs(properties.map((property, index) => ([property, v[index]]))));
 };
 
-const isValidData = () => {
-  if (!dataStore || !updateTime) {
-    return false;
-  }
-
-  const diffTime = +new Date() - updateTime;
-  return diffTime < timeToLive;
-};
-
-const updateData = (data) => {
-  dataStore = data;
-  updateTime = +new Date();
-};
-
 const getSheetValues = (valueRanges, rangeName) =>
   valueRanges.find(v => new RegExp(rangeName).test(v.range)).values;
 
-const getData = async () => {
-  if (isValidData()) {
-    return Promise.resolve(dataStore);
-  }
+const getClient = async (keyFile, scopes) => google.auth.getClient({
+  keyFile,
+  scopes,
+});
 
+class CustomMap extends Map {
+  get(key) {
+    if (!this.has(key)) {
+      return;
+    }
+
+    const result = super.get(key);
+
+    if ((Date.now() - result._date.now()) >= TEN_MINUTES) {
+      super.delete(key);
+    }
+
+    return result;
+  }
+}
+
+_.memoize.Cache = CustomMap;
+
+const getSheets = _.memoize(async (spreadsheetId) => { // TEAM_SHEET_ID
   try {
-    const client = await google.auth.getClient({
-      keyFile: CREDS_FILE,
-      scopes,
-    });
+    const client = await getClient(CREDS_FILE, googleScopes);
 
     const sheets = google.sheets('v4');
 
     const table = await sheets.spreadsheets.values.batchGet({
       auth: client,
-      spreadsheetId: TEAM_SHEET_ID,
+      spreadsheetId,
       ranges,
     });
 
+    const result = _.get(table, 'data', {});
+    result._date = new Date();
 
-    const team = getSheetValues(table.data.valueRanges, TEAM_RANGE);
-    const careers = getSheetValues(table.data.valueRanges, CAREERS_RANGE);
+    return result;
+  } catch (err) {
+    return { message: err.message, err };
+  }
+});
 
-    const data = {
-      team: arrayOfArraysToCollection(team),
-      careers: arrayOfArraysToCollection(careers),
-    };
+const getData = async (pageName, sheetId) => {
+  const start = Date.now();
+  console.log('!!! Start !!! ', pageName);
+  const sheetData = await getSheets(sheetId);
+  console.log('End: ', Date.now() - start);
 
-    updateData(data);
-    return Promise.resolve(dataStore);
-  } catch (e) {
-    console.error(e);
-    return null;
+  console.log('sheetData: ', sheetData);
+
+  if (sheetData.err) {
+    console.error('[Google api error]: ', sheetData);
+
+    return Promise.reject(null);
+  }
+
+  switch (pageName) {
+    case 'team': {
+      const team = getSheetValues(sheetData.valueRanges, TEAM_RANGE);
+
+      return arrayOfArraysToCollection(team);
+    }
+    case 'careers': {
+      const careers = getSheetValues(sheetData.valueRanges, CAREERS_RANGE);
+
+      return arrayOfArraysToCollection(careers);
+    }
+    default: {
+      const team = getSheetValues(sheetData.valueRanges, TEAM_RANGE);
+
+      return arrayOfArraysToCollection(team);
+    }
   }
 };
 
-getData();
+// Do I need it?
+getData('team', TEAM_SHEET_ID);
 
-const getTeam = async () => {
-  const d = await getData();
-  return d.team;
-};
-const getCareers = async () => {
-  const d = await getData();
-  return d.careers;
-};
+const getTeam = async () => getData('team', TEAM_SHEET_ID);
+const getCareers = async () => getData('careers', TEAM_SHEET_ID);
 
 module.exports = {
   getTeam,
